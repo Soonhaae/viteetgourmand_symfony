2e193548861183574e08d3f9720c95fd52ed2f93
Soonhaae <raffaelli.claire@gmail.com>
2026-04-25 15:44:41 +0200

    feat: complete Menu entity

--- src/Entity/Menu.php
+++ src/Entity/Menu.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\MenuRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: MenuRepository::class)]
@@ -14,16 +15,25 @@ class Menu
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
-    private ?string $title = null;
+    private string $title;
 
     #[ORM\Column(length: 255)]
-    private ?string $theme = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $description = null;
+    private string $theme;
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $regime = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $content = null;
+
+    #[ORM\Column]
+    private ?int $minPersons = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
+    private ?string $price = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $conditions = null;
 
     public function getId(): ?int
     {
@@ -54,18 +64,6 @@ class Menu
         return $this;
     }
 
-    public function getDescription(): ?string
-    {
-        return $this->description;
-    }
-
-    public function setDescription(string $description): static
-    {
-        $this->description = $description;
-
-        return $this;
-    }
-
     public function getRegime(): ?string
     {
         return $this->regime;
@@ -74,6 +72,54 @@ class Menu
     public function setRegime(?string $regime): static
     {
         $this->regime = $regime;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(?string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getMinPersons(): ?int
+    {
+        return $this->minPersons;
+    }
+
+    public function setMinPersons(int $minPersons): static
+    {
+        $this->minPersons = $minPersons;
+
+        return $this;
+    }
+
+    public function getPrice(): ?string
+    {
+        return $this->price;
+    }
+
+    public function setPrice(string $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getConditions(): ?string
+    {
+        return $this->conditions;
+    }
+
+    public function setConditions(string $conditions): static
+    {
+        $this->conditions = $conditions;
 
         return $this;
     }
