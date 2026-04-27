6a12d1301eb0beaf32a22ccc1416170fa1beb352
Soonhaae <raffaelli.claire@gmail.com>
2026-04-27 11:10:21 +0200

    feat: add menus display

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,14 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Menu;
 
 final class IndexController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
+        $menus = $em->getRepository(Menu::class)->findAll();
+    
         return $this->render('index/index.html.twig', [
             'controller_name' => 'Mon controller index',
+            'menus' => $menus
         ]);
     }
 }
