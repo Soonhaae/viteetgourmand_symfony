e0df963dad6c7088a16261d46aba3232c2b2c8d0
Soonhaae <raffaelli.claire@gmail.com>
2026-05-05 12:36:29 +0200

    refactor: simplify IndexController for homepage

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,19 +5,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Doctrine\ORM\EntityManagerInterface;
-use App\Entity\Menu;
+
 
 final class IndexController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
-    public function index(EntityManagerInterface $em): Response
+     public function index(): Response
     {
-        $menus = $em->getRepository(Menu::class)->findAll();
-    
-        return $this->render('index/index.html.twig', [
-            'controller_name' => 'Mon controller index',
-            'menus' => $menus
-        ]);
+        return $this->render('index/index.html.twig');
     }
+    
 }
